6910cace3a9946f182293992c8ee28045a80ee69
Thom212 <android212@gmail.com>
2018-05-23 03:29:23 +0200

    améliorations cosmétiques et messages feedback

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,35 +25,47 @@ reservationFront.config(['$routeProvider',
   var $ctrl = this;
 
   $ctrl.heures = [8,9,10,11,12,13,14,15,16,17,18,19]; // ["8h","9h"]
-  $ctrl.formData = {};
   $ctrl.salles = [];
   $ctrl.filterDate;
   $ctrl.filterHeure;
-  $ctrl.showInfoMessage = true;
+  $ctrl.showListeSalles = false;
+  $ctrl.showMessageSuccess = false;
+  $ctrl.showMessageError = false;
 
-
-  
   $ctrl.updateSalles = function() {
+    $ctrl.showMessageSuccess = false;
+    $ctrl.showMessageError = false;
     if( $ctrl.filterDate == null || $ctrl.filterHeure == null ) {
-      $ctrl.showInfoMessage = true;
+      $ctrl.showListeSalles = false;
     } else {
-      $ctrl.showInfoMessage = false;
       const formatedDate = $filter('date')( $ctrl.filterDate, 'yyyy-MM-dd');
       ReservationService.getSalles(formatedDate, $ctrl.filterHeure).then(function(response) {
         $ctrl.salles = response.data;
+        $ctrl.showListeSalles = true;
       });
     } 
   }
  
   $ctrl.reserverSalle = function(salle_id) {
+    if( $ctrl.filterDate == null || $ctrl.filterHeure == null ) {
+      $ctrl.showListeSalles = false;
+    } else {
+      const formatedDate = $filter('date')( $ctrl.filterDate, 'yyyy-MM-dd');
+      ReservationService.addReservation(salle_id, formatedDate, $ctrl.filterHeure)
+      .then(function(response) {
+        $ctrl.updateSalles();
+        $ctrl.showMessageSuccess = true;
 
-    const formatedDate = $filter('date')( $ctrl.filterDate, 'yyyy-MM-dd');
-
-    ReservationService.addReservation(salle_id, formatedDate, $ctrl.filterHeure).then(function(response) {
-      // $scope.salles.push($scope.formData)
-      // $scope.formData = {};
-      $ctrl.updateSalles();
-    });
+        $ctrl.message = "Votre réservation a bien été prise en compte"
+      })
+      .catch(function(error) {
+        $ctrl.updateSalles();
+        console.log(JSON.stringify(error));
+        if(error.status == 422) {
+          $ctrl.showMessageError = true;
+        }
+      });
+    }
   }
 
 }]);
